b41d3845666b76f32a3621205744c1732110588b
Bafedile <ddimakatso30@gmail.com>
2021-08-27 10:14:00 +0200

    Added the display panel it displays the items in store

--- src/za/ac/tut/pizzashop/DisplayPanel.java
+++ src/za/ac/tut/pizzashop/DisplayPanel.java
@@ -6,15 +6,13 @@
 package za.ac.tut.pizzashop;
 
 import java.awt.Color;
-import java.awt.Graphics;
-import java.awt.Image;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-import javax.imageio.ImageIO;
+import java.awt.Font;
+import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.border.Border;
 import javax.swing.border.LineBorder;
 
 /**
@@ -23,6 +21,13 @@ import javax.swing.border.LineBorder;
  */
 public class DisplayPanel extends JPanel {
     JLabel label = new JLabel("Items Menu");
+    JTextArea text = new JTextArea("Buy Any Two \n\n"
+            + "Or More Large Pizzas\n\n"
+            + "And Get 12% OFF\n\n"
+            + "Your Purchase\n\n"
+            + "@Superio Code Pizza \n\n\n"
+            + "We Care For Our Customers!!");
+    
 //    BufferedImage beefAndCheese ;
     JLabel beef = new JLabel(new ImageIcon("beef_and_cheese.jpg"));
     JLabel mashroom = new JLabel(new ImageIcon("mushroompizza.jpg"));
@@ -31,40 +36,46 @@ public class DisplayPanel extends JPanel {
     JLabel mashroomLabel = new JLabel("Chicken And Mushroom");
     JLabel beefLabel = new JLabel("Beef And Cheese");
     JLabel mayoLabel = new JLabel("Chicken And Mayo");
-    
+    Border border = BorderFactory.createLineBorder(Color.magenta, 25);
     public DisplayPanel(){
         this.setLayout(null);
-        this.setBackground(Color.gray);
-        this.setBorder(new LineBorder(Color.BLACK));
+        this.setBackground(Color.lightGray);
+        this.setBorder(border);
         this.setBounds(0,0,850,250);
-        this.label.setBounds(325,0,200,30);
-        
+        this.label.setBounds(325,0,90,30);
+        this.label.setForeground(Color.WHITE);
+        this.label.setBorder(new LineBorder(Color.magenta));
         
         
         this.beef.setBounds(10,40,200,180);
         this.beefLabel.setBounds(20,220,200,20);
+        this.beefLabel.setForeground(Color.WHITE);
         
-        this.mashroom.setBounds(250,40,200,180);
+        this.mashroom.setBounds(240,40,200,180);
         this.mashroomLabel.setBounds(260,220,200,20);
+        this.mashroomLabel.setForeground(Color.WHITE);
         
-        this.mayo.setBounds(500,40,200,180);
+        this.mayo.setBounds(470,40,200,180);
         this.mayoLabel.setBounds(510,220,200,20);
+        this.mayoLabel.setForeground(Color.WHITE);
+        
+        this.text.setBounds(670,20,200,200);
+        this.text.setBackground(Color.lightGray);
+        this.text.setBorder(new LineBorder(Color.CYAN));
+        this.text.setFont(new Font("Verdana",Font.ITALIC,17));
+        this.text.setForeground(Color.BLUE);
+        this.text.setEditable(false);
         
         // add the features
         this.add(label);
+        this.add(text);
         this.add(beef);
         this.add(mashroom);
         this.add(mayo);
         this.add(beefLabel);
         this.add(mayoLabel);
         this.add(mashroomLabel);
-//        this.setBackground(Color.GRAY);
         this.setVisible(true);
     }
-    
-//    @Override
-//    protected void paintComponent(Graphics g){
-//        super.paintComponent(g);
-//        g.drawImage(beefAndCheese,30,30,this);
-//    }
+
 }
